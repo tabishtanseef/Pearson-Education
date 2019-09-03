7c381392beca94d248e7fc9dfdcbcb8f4d6e78cb
Tabish Tanseef <tabishtanseef@gmail.com>
2019-09-03 17:22:49 +0530

    pearson education sample

---
+++ data/config.js
@@ -0,0 +1,42 @@
+var fPage = 1;
+						 const appdb = {
+						'config': {
+							'subject': 'Pearson Education',
+							'class': 'Class 6',
+							'id': 'gud_507',
+							'totalPages': 3,
+							'bookWidth': 2570,
+							'bookHeight': 3300
+						},
+						'ebook': {'toc': {
+							'icon': 'img/content.png',
+							'menu': 'Table of Contents',
+							'link': 'content',
+							'data': [{
+						'page': 1,
+						'title': 'Chapter 1'
+					}]},'Interactivities': {
+								'icon': 'img/interactivity.png',
+								'menu': 'Interactivities',
+								'link': 'iframe',
+								'data': [
+									{
+								'path': '',
+								'title': 'Activity',
+								'page': 2,
+								'interactivities':[
+									{
+							'path': 'resources/Activity/Act-1/index.html',
+							'title': 'Activity - 1',
+							'size': '1200x2500',
+							'page': 2}
+									],
+								'size': '1200x2500',
+								}
+								]}
+								,'zother': [
+							
+						]
+						
+					}
+				}
---
+++ data/page.js
@@ -0,0 +1,9 @@
+const PAGES_LINKING = {'2':[{			 
+											x: 45,
+											y: 7,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Act-1/index.html',
+											type: 'iframe',
+											'size': '1200x2500'
+											}]}
