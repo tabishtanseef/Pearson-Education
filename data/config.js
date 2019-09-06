ba76731dd4efce7c8c051710f4494995c8e3ab45
Tabish Tanseef <tabishtanseef@gmail.com>
2019-09-06 18:16:42 +0530

    with activities

--- data/config.js
+++ data/config.js
@@ -3,8 +3,8 @@ var fPage = 1;
 						'config': {
 							'subject': 'Pearson Education',
 							'class': 'Class 6',
-							'id': 'gud_507',
-							'totalPages': 3,
+							'id': 'gud_349',
+							'totalPages': 10,
 							'bookWidth': 2570,
 							'bookHeight': 3300
 						},
@@ -23,15 +23,47 @@ var fPage = 1;
 									{
 								'path': '',
 								'title': 'Activity',
-								'page': 2,
+								'page': 6,
 								'interactivities':[
 									{
-							'path': 'resources/Activity/Act-1/index.html',
+							'path': 'resources/Activity/Page-1/index.html',
 							'title': 'Activity - 1',
-							'size': '1200x2500',
-							'page': 2}
+							'size': '1200x2000',
+							'page': 1},{
+							'path': 'resources/Activity/Page-2/index.html',
+							'title': 'Activity - 2',
+							'size': '1200x2000',
+							'page': 2},{
+							'path': 'resources/Activity/Page-3/index.html',
+							'title': 'Activity - 3',
+							'size': '1200x2000',
+							'page': 3},{
+							'path': 'resources/Activity/Page-4/index.html',
+							'title': 'Activity - 4',
+							'size': '1200x2000',
+							'page': 4},{
+							'path': 'resources/Activity/Page-4_2/index.html',
+							'title': 'Activity - 5',
+							'size': '1200x2000',
+							'page': 4},{
+							'path': 'resources/Activity/Page-5/index.html',
+							'title': 'Activity - 6',
+							'size': '1200x2000',
+							'page': 5},{
+							'path': 'resources/Activity/Page-5_2/index.html',
+							'title': 'Activity - 7',
+							'size': '1200x2000',
+							'page': 5},{
+							'path': 'resources/Activity/Page-6/index.html',
+							'title': 'Activity - 8',
+							'size': '1200x2000',
+							'page': 6},{
+							'path': 'resources/Activity/Page-6_2/index.html',
+							'title': 'Activity - 9',
+							'size': '1200x2000',
+							'page': 6}
 									],
-								'size': '1200x2500',
+								'size': '1200x2000',
 								}
 								]}
 								,'zother': [
--- data/page.js
+++ data/page.js
@@ -3,7 +3,71 @@ const PAGES_LINKING = {'2':[{
 											y: 7,
 											title: 'Activity',
 											icon: 'img/icons/Act.png',
-											link: 'resources/Activity/Act-1/index.html',
+											link: 'resources/Activity/Page-2/index.html',
 											type: 'iframe',
-											'size': '1200x2500'
+											'size': '1200x2000'
+											}],'1':[{			 
+											x: 36,
+											y: 75,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-1/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'3':[{			 
+											x: 4,
+											y: 32,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-3/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'4':[{			 
+											x: 93,
+											y: 13,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-4/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											},{			 
+											x: 91,
+											y: 38,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-4_2/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'5':[{			 
+											x: 4,
+											y: 13,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-5/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											},{			 
+											x: 93,
+											y: 52,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-5_2/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'6':[{			 
+											x: 93,
+											y: 13,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-6/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											},{			 
+											x: 4,
+											y: 60,
+											title: 'Activity',
+											icon: 'img/icons/Act.png',
+											link: 'resources/Activity/Page-6_2/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
 											}]}
